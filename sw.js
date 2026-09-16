f26643e46f7d7d99c096984c6e6410aaa2979f24
brunapupo-cloud <brunapupo@hotmail.com>
2026-09-16 11:46:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='qualyhouse-alpha-v14';
+const CACHE='qualyhouse-alpha-v15';
 const ASSETS=['./','index.html','manifest.webmanifest','assets/villa-park-terreo.jpeg','assets/villa-park-superior.jpeg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
