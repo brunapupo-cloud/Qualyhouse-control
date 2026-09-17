79d0fe5b1d3e6fd4598d425511066a406a7ac66d
brunapupo-cloud <brunapupo@hotmail.com>
2026-09-17 17:20:55 -0300

    Refresh Qualyhouse loading screen cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='qualyhouse-alpha-v18';
+const CACHE='qualyhouse-alpha-v19';
 const ASSETS=['./','index.html','manifest.webmanifest','cloud.js','auth-ui.js','assets/villa-park-terreo.jpeg','assets/villa-park-superior.jpeg'];
 
 self.addEventListener('install',e=>{
