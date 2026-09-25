ff5cadf751a0a1405233592454aa26deb8a1d612
brunapupo-cloud <brunapupo@hotmail.com>
2026-09-25 10:08:33 -0300

    Refresh cache for parking summary

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='qualyhouse-alpha-v24';
+const CACHE='qualyhouse-alpha-v25';
 const ASSETS=['./','index.html','manifest.webmanifest','cloud.js','auth-ui.js','assets/villa-park-terreo.jpeg','assets/villa-park-superior.jpeg'];
 
 self.addEventListener('install',e=>{
